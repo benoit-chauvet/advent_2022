bb0d31f7a6815ae7a68b59cd9b09502491d26e67
Benoit Chauvet <contact@benoitchauvet.com>
2022-12-08 13:42:54 +0100

    day 8 peding

--- src/day_8.rs
+++ src/day_8.rs
@@ -4,12 +4,13 @@ pub fn day8() {
     let path = String::from("files/day_8.txt");
     let lines: Vec<String> = file_utils::get_lines_reader(path);
 
-    const HEIGHT: usize = 5; //99; // lines.len();
-    const WIDTH: usize = 5; //99; //lines[0].trim().len();
+    const HEIGHT: usize = 5;// 99; // lines.len();
+    const WIDTH: usize = 5;//99; //lines[0].trim().len();
 
     println!("h:{} w:{}", HEIGHT, WIDTH);
 
     let mut forest: [[i32; WIDTH]; HEIGHT] = [[0; WIDTH]; HEIGHT];
+    let mut counted: [[bool; WIDTH]; HEIGHT] = [[false; WIDTH]; HEIGHT];
 
     for i in 0..lines.len() {
         let line = &lines[i];
@@ -21,84 +22,81 @@ pub fn day8() {
         }
     }
 
-    let mut visible_trees = (WIDTH * 2) + (HEIGHT * 2) - 4;
-    println!("{}", visible_trees);
+    let mut visible_trees = 0;
 
-    for i in 1..WIDTH - 1 {
-        let mut highest_top = forest[i][0];
-        let mut highest_bottom = forest[i][HEIGHT - 1];
-
-        for j in 1..HEIGHT - 1 {
-            let mut highest_left = forest[0][j];
-            let mut highest_right = forest[WIDTH - 1][j];
-
-            for y in 0..j - 1 {
-                if forest[i][y] > highest_top {
-                    highest_top = forest[i][y];
+    let mut max = -1;
+    for i in 0..WIDTH {
+        max = -1;
+        for j in 0..HEIGHT {
+            if forest[i][j] > max {
+                if !counted[i][j]{
+                    visible_trees+=1;
+                    counted[i][j] = true;
                 }
+                max = forest[i][j];
             }
-
-            for y in (j - 1..HEIGHT - 1).rev() {
-                if forest[i][y] > highest_bottom {
-                    highest_bottom = forest[i][y];
-                }
-            }
-
-            for x in 0..i - 1 {
-                if forest[x][j] > highest_left {
-                    highest_left = forest[x][j];
-                }
-            }
-
-            for x in (i - 1..WIDTH - 1).rev() {
-                if forest[x][j] > highest_right {
-                    highest_right = forest[x][j];
-                }
-            }
-
-            let mut counted = false;
-            // TOP
-            if forest[i][j] > highest_top {
-                visible_trees += 1;
-                counted = true;
-                highest_top = forest[i][j];
-            }
-            // BOTTOM
-            else if forest[i][j] > highest_bottom {
-                visible_trees += 1;
-                counted = true;
-                highest_bottom = forest[i][j];
-            }
-            // LEFT
-            else if forest[i][j] > highest_left {
-                visible_trees += 1;
-                counted = true;
-                highest_left = forest[i][j];
-            }
-            // RIGHT
-            else if forest[i][j] > highest_right {
-                visible_trees += 1;
-                counted = true;
-                highest_right = forest[i][j];
-            }
-            if (counted) {
-                print!("v");
-            } else {
-                print!("-");
-            }
+            // else
+            // {
+            //     break;
+            // }
         }
-
-        println!();
     }
 
-    println!("VISIBLE TREES : {}", visible_trees);
+    for i in (0..WIDTH).rev() {
+        max = -1;
+        for j in 0..HEIGHT {
+            if forest[i][j] > max {
+                if !counted[i][j]{
+                    visible_trees+=1;
+                    counted[i][j] = true;
+                }
+                max = forest[i][j];
+            }
+            // else
+            // {
+            //     break;
+            // }
+        }
+    }
 
-    // x x x x x x
-    // x x x x x x
-    // x x x x x x
-    // x x x x x x
-    // x x x x x x
-    // x x x x x x
+    for i in (0..WIDTH).rev() {
+        max = -1;
+        for j in (0..HEIGHT).rev() {
+            if forest[i][j] > max {
+                if !counted[i][j]{
+                    visible_trees+=1;
+                    counted[i][j] = true;
+                }
+                max = forest[i][j];
+            }
+            // else
+            // {
+            //     break;
+            // }
+        }
+    }
+
+    for i in 0..WIDTH {
+        max = -1;
+        for j in (0..HEIGHT).rev() {
+            if forest[i][j] > max {
+                if !counted[i][j]{
+                    visible_trees+=1;
+                    counted[i][j] = true;
+                }
+                max = forest[i][j];
+            }
+            // else
+            // {
+            //     break;
+            // }
+        }
+    }
+
+
+
+
+    println!("VISIBLE TREES : {}", visible_trees);
 
     // for i in 0..height {
     //     for j in 0..width {
