b31cf55ed3d7956777daac40ec511bc680c14cc1
Benoit Chauvet <contact@benoitchauvet.com>
2022-12-16 08:22:55 +0100

    day 13 part 1 \o/

--- src/day_13.rs
+++ src/day_13.rs
@@ -2,10 +2,31 @@ use crate::file_utils;
 use json::JsonValue;
 
 //5563 : too low
-
+// 5710: ko
 //5819 : too high
 
 pub fn day13() {
+    part1();
+    part2();
+}
+
+pub fn part2() {
+    let file_path = String::from("files/day_13.txt");
+    let lines: Vec<String> = file_utils::get_lines_reader(file_path);
+
+    let mut inputs: Vec<JsonValue> = Vec::new();
+
+    for i in (0..lines.len()).step_by(3) {
+        inputs.push(json::parse(&lines[i]).unwrap());
+        inputs.push(json::parse(&lines[i + 1]).unwrap());
+    }
+
+    Sort(inputs);
+}
+
+pub fn Sort(inputs: Vec<JsonValue>) {}
+
+pub fn part1() {
     let file_path = String::from("files/day_13.txt");
     let lines: Vec<String> = file_utils::get_lines_reader(file_path);
 
@@ -18,13 +39,9 @@ pub fn day13() {
         let left_input = json::parse(&lines[i]).unwrap();
         let right_input = json::parse(&lines[i + 1]).unwrap();
 
-        let result = compare_arrays(&left_input, &right_input);
+        let result = compare(&left_input, &right_input);
 
-        //if result == ComparisonResult::Unhandled {
-        println!("{:?} index {}", result, index);
-        println!("left : {}", left_input);
-        println!("right : {}", right_input,);
-        //}
+        //println!("{:?} {}", result, index);
 
         if result == ComparisonResult::Ok {
             right_indexes.push(index);
@@ -40,62 +57,63 @@ pub fn day13() {
     println!("TOTAL: {}", total);
 }
 
-fn compare_arrays(left_arr: &JsonValue, right_arr: &JsonValue) -> ComparisonResult {
-    let mut result = ComparisonResult::Unhandled;
+fn compare(left: &JsonValue, right: &JsonValue) -> ComparisonResult {
+    if left.is_number() && right.is_number() {
+        return compare_numbers(left, right);
+    } else if left.is_array() && right.is_array() {
+        return compare_arrays(left, right);
+    } else if left.is_array() && right.is_number() {
+        let r = right.as_u32();
+        let mut arr = Vec::<JsonValue>::new();
+        arr.push(r.into());
+        return compare_arrays(left, &JsonValue::Array(arr));
+    } else if left.is_number() && right.is_array() {
+        let l = left.as_u32();
+        let mut arr = Vec::<JsonValue>::new();
+        arr.push(l.into());
+        return compare_arrays(&JsonValue::Array(arr), right);
+    } else if right.is_empty() {
+        return ComparisonResult::Error;
+    } else {
+        println!("?? edge case ??");
+        return ComparisonResult::Error;
+    }
+}
 
-    if left_arr.len() == 0 {
-        return ComparisonResult::Ok;
+fn compare_arrays(left: &JsonValue, right: &JsonValue) -> ComparisonResult {
+    if left.len() == 0 && right.len() == 0 {
+        return ComparisonResult::Continue;
     }
 
-    for i in 0..left_arr.len() {
-        let left = &left_arr[i];
+    let mut carry_on = false;
 
-        if right_arr.len() == 0 {
+    for i in 0..left.len() {
+        // finished right before left : error :
+        if right.len() < i {
             return ComparisonResult::Error;
         }
 
-        let right = &right_arr[i];
-
-        // println!("left : {} - right : {}", left, right);
-
-        if left.is_number() && right.is_number() {
-            result = compare_numbers(left, right);
-        }
-        if left.is_array() && right.is_array() {
-            result = compare_arrays(left, right);
-        }
-        if left.is_array() && right.is_number() {
-            let r = right.as_u32();
-            let mut arr = Vec::<JsonValue>::new();
-            arr.push(r.into());
-            result = compare_arrays(left, &JsonValue::Array(arr));
-        }
-        if left.is_number() && right.is_array() {
-            let l = left.as_u32();
-            let mut arr = Vec::<JsonValue>::new();
-            arr.push(l.into());
-            result = compare_arrays(&JsonValue::Array(arr), right);
-        }
+        let result = compare(&left[i], &right[i]);
 
         if result != ComparisonResult::Continue {
             return result;
         }
+        carry_on = true;
     }
 
-    if left_arr.len() > right_arr.len() {
-        return ComparisonResult::Error;
-    } else {
-        return ComparisonResult::Ok;
+    if carry_on && left.len() == right.len() {
+        return ComparisonResult::Continue;
     }
 
-    //return result;
+    // finished left before right : Ok :
+    return ComparisonResult::Ok;
 }
 
 fn compare_numbers(left: &JsonValue, right: &JsonValue) -> ComparisonResult {
     let l = left.as_u32().unwrap();
     let r = right.as_u32().unwrap();
 
-    //println!("compare numbers : {} {}", l, r);
+    // println!("{} {}", l, r);
 
     if l < r {
         return ComparisonResult::Ok;
@@ -113,5 +131,5 @@ enum ComparisonResult {
     Continue,
     Ok,
     Error,
-    Unhandled,
+    //Unhandled,
 }
